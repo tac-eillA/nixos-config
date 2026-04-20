1722620f36f8145e6525109ebfacff5cac96d491
Allison Snodgrass <email@allie.is>
2026-04-20 16:16:11 -0700

    remove auto flatpak installs to see whats going on

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkg, ... }:
 {
   #imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
   services = {
@@ -7,15 +7,15 @@
 
       # List the Flatpak applications you want to install
       # Use the official Flatpak application ID (e.g., from flathub.org)
-      packages = [
-        "com.github.tchx84.Flatseal"      # Manage flatpak permissions - should always have this
-        "io.github.flattool.Warehouse"    # Manage flatpaks, clean data, remove flatpaks and deps
-        "app.opencomic.OpenComic"         # Comic and Manga reader
-        "com.discordapp.Discord"
-        "com.core447.StreamController"
+      #packages = [
+      #  "com.github.tchx84.Flatseal"      # Manage flatpak permissions - should always have this
+      #  "io.github.flattool.Warehouse"    # Manage flatpaks, clean data, remove flatpaks and deps
+      #  "app.opencomic.OpenComic"         # Comic and Manga reader
+      #  "com.discordapp.Discord"
+      #  "com.core447.StreamController"
 
         # Add other Flatpak IDs here, e.g., "org.mozilla.firefox"
-      ];
+      #];
 
       # Optional: Automatically update Flatpaks when you run nixos-rebuild swit ch
       update.onActivation = true;
