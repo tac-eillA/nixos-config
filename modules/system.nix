67ecc464d7e04a68d4ed51bd975e8a1bcfae0006
Allison Snodgrass <email@allie.is>
2026-04-20 17:20:30 -0700

    add additional cachix substituters

--- modules/system.nix
+++ modules/system.nix
@@ -9,10 +9,16 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org/"
+        "https://chaotic-nyx.cachix.org/"
+        "https://cachix.cachix.org"
+        "https://nix-gaming.cachix.org/"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       ];
       experimental-features = [
         "nix-command"
