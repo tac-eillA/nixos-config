f1cc9201e86fb7ea7ed4477f9334ae271e5532c1
Allison Snodgrass <email@allie.is>
2026-04-24 01:43:01 -0700

    Update proxy.nix

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -15,7 +15,7 @@ in
       enable = true;
       tunnels = {
         "8eaa3da2-b2ae-4cbf-86f0-73bda6de85bd" = {
-          credentialsFile = "~/.cloudflared/";
+          credentialsFile = "~/.cloudflared/cert.pem";
           ingress = {
             "git.allie.sh" = {
               service = "http://${forgejoHost}:3000";
