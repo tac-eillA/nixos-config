ed57b92aa7ba5941665d01625b1a949f78f7e524
Allison Snodgrass <email@allie.is>
2026-04-27 14:25:13 -0700

    fix update alias

--- modules/core/shell.nix
+++ modules/core/shell.nix
@@ -20,7 +20,7 @@
       nixc = "cd ~/nixos-config";
       rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#$(hostname)";
       rebuild-boot = "sudo nixos-rebuild boot --flake ~/nixos-config#$(hostname)";
-      update = "sudo nix flake update ~/nixos-config#$(hostname)";
+      update = "nix flake update --flake ~/nixos-config";
       garbage = "sudo nix-collect-garbage -d";
       c = "clear";
     };
