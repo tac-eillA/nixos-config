dd55d66c44a3120767cf62220b721674cab43f6a
Allison Snodgrass <email@allie.is>
2026-04-28 00:58:00 -0700

    Update unreal.nix

--- modules/dev/unreal.nix
+++ modules/dev/unreal.nix
@@ -605,7 +605,6 @@ in
     };
 
     boot.kernel.sysctl = lib.mkIf cfg.enableKernelTweaks {
-      "fs.inotify.max_user_watches" = lib.mkDefault 1048576;
       "vm.max_map_count" = lib.mkDefault 1048576;
     };
 
