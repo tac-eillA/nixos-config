51310cc8fa1c4bb25bca3172438fc517e998e24d
Allison Snodgrass <email@allie.is>
2026-04-28 01:34:02 -0700

    add test settings for unreal

--- hosts/athena/configuration.nix
+++ hosts/athena/configuration.nix
@@ -23,5 +23,26 @@
       enableGamemode = true;
     };
 
+    # programs.unrealEnginePrebuilt = {
+    #     enable = true;
+    #     user = "allison";
+
+    #     # This is where ue-bin-install will place the extracted engine by default.
+    #     engineRoot = "$HOME/Applications/UnrealEngine";
+
+    #     projectsDir = "$HOME/UnrealProjects";
+
+    #     # For athena, probably "nvidia" if that is your NVIDIA desktop.
+    #     gpu = "nvidia";
+
+    #     # Your repo already has gaming/dev modules, so these are safe defaults.
+    #     enableCppTools = true;
+    #     installCppToolsGlobally = false;
+    #     enableGamemode = true;
+    #     enableNixLd = true;
+    #     enableGraphics = true;
+    #     enableAudio = true;
+    #   };
+
   networking.hostName = "athena";
 }
