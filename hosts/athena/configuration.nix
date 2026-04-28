d47d79d94cfb49b1fa0f9546afacf36f807b8bdc
Allison Snodgrass <email@allie.is>
2026-04-28 00:42:03 -0700

    move ue to nix module rather than a shell flake

--- hosts/athena/configuration.nix
+++ hosts/athena/configuration.nix
@@ -4,7 +4,24 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/profiles/workstation.nix
+    ../../modules/dev/unreal.nix
   ];
+
+  programs.unrealEngine = {
+      enable = true;
+      user = "allison";
+
+      # Use "nvidia" or "amd"
+      # Use "generic" if unsure.
+      gpu = "amd";
+
+      engineRoot = "$HOME/src/UnrealEngine";
+      projectsDir = "$HOME/UnrealProjects";
+      engineRef = "release";
+      jobs = null;
+      enableSteamCompat = false;
+      enableGamemode = true;
+    };
 
   networking.hostName = "athena";
 }
