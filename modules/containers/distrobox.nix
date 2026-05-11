16dc37860db5340928772f777b8ece7bb0971b0e
Allison Snodgrass <email@allie.is>
2026-05-10 20:15:53 -0700

    update distrobox and add rocky8 container config for unreal dev

--- modules/containers/distrobox.nix
+++ modules/containers/distrobox.nix
@@ -1,5 +1,31 @@
 { pkgs, ... }:
 
+let
+  username = 'allison';
+
+  unrealDevAliases = pkgs.writeText "unreal-dev-aliases.sh" ''
+    alias cproj='cd /workspace/projects'
+  '';
+
+  distroboxIni = ''
+    [unreal-dev]
+    additional_packages="alsa-lib pango libxkbcommon libgbm libXrandr libXdamage libXcomposite-devel at-spi2-atk libxml2-devel nss.x86_64"
+    image=rockylinux:8
+    init=false
+    pull=true
+    root=false
+    replace=false
+    start_now=false
+
+    home=/home/${username}/.local/share/distrobox/homes/unreal-dev
+
+    volume="/home/${username}/Projects:/workspace/projects"
+    volume="${unrealDevAliases}:/opt/distrobox-aliases/aliases.sh:ro"
+
+    init_hooks="grep -qxF 'source /opt/distrobox-aliases/aliases.sh' ~/.bashrc || echo 'source /opt/distrobox-aliases/aliases.sh' >> ~/.bashrc"
+    init_hooks="grep -qxF 'source /opt/distrobox-aliases/aliases.sh' ~/.zshrc || echo 'source /opt/distrobox-aliases/aliases.sh' >> ~/.zshrc"
+  '';
+in
 {
   virtualisation = {
     oci-containers.backend = "podman";
@@ -20,4 +46,10 @@
     podman-compose
     passt
   ];
+
+  services.flatpak.packages = [
+    "io.github.dvlv.boxbuddyrs"
+  ];
+
+  environment.etc."distrobox/distrobox.ini".text = distroboxIni;
 }
